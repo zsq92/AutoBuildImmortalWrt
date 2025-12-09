3c59392434bfead356f5031597081c4b7d1c730f
wukongdaily <2666180@gmail.com>
2025-12-09 15:01:29 +0800

    Comment out IPSec VPN server package line

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -53,3 +53,5 @@
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 # 统一文件共享
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES webdav2 luci-app-unishare"
+# IPSec VPN 服务器
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
