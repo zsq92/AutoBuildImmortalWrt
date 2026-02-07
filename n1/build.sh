9d0b81ebebda5354e851f50d5d9a579c8d510255
wukongdaily <2666180@gmail.com>
2026-02-07 17:04:51 +0800

    Remove luci-i18n-dufs-zh-cn package from build script
    Removed the package 'luci-i18n-dufs-zh-cn' from the list of packages to install.

--- n1/build.sh
+++ n1/build.sh
@@ -29,8 +29,6 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
-# 静态文件服务器dufs(推荐)
-PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
